0fe7767e873c77a578755216642cb3adef7e3c23
emmanuelagyapong <emmanuelagyapong070@gmail.com>
2020-06-03 00:09:06 +0000

    progress

--- Service_1/application/routes.py
+++ Service_1/application/routes.py
@@ -2,9 +2,9 @@ from application import app
 from flask import render_template, request
 import requests
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    result = requests.get('http://35.223.230.68:5003/fullname')
+    result = requests.get('http://service_4:5003/fullname')
     fullname = result.text
     print(fullname)
     return render_template('home.html', fullname=fullname, title='Home page')
