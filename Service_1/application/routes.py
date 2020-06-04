9618dac996b1637ddf3e536062b2b2bfb47f10f1
emmanuelagyapong <emmanuelagyapong070@gmail.com>
2020-06-04 21:50:06 +0000

    jenkins

--- Service_1/application/routes.py
+++ Service_1/application/routes.py
@@ -1,12 +1,17 @@
-from application import app
+from application import app, db
 from flask import render_template, request
 import requests
+from application.models import Names
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     result = requests.get('http://service_4:5003/fullname')
-    fullname = result.text
-    print(fullname)
-    return render_template('home.html', fullname=fullname, title='Home page')
-    
+    fullname1 = result.text
+    fullnamedb= Names(fullname=fullname1)
 
+    db.session.add(fullnamedb)
+    db.session.commit()
+
+    namesData=Names.query.all()
+    #print(fullname)
+    return render_template('home.html', fullname=fullname1, posts=namesData, title='Home page')
