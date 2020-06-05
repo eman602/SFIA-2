16da08325cae9122caa7ab23bae3741d5215331f
emmanuelagyapong <emmanuelagyapong070@gmail.com>
2020-06-05 08:48:16 +0000

    jenkins

--- script/before_installation.sh
+++ script/before_installation.sh
@@ -2,4 +2,4 @@
 sudo apt update -y
 sudo apt install python3 python3-venv python3-pip -y
 python3 -m venv venv
-source ~/bashrc
+
--- script/docker.sh
+++ script/docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source ~/bashrc
-docker  deploy --compose-file docker-compose.yaml up
+docker  --compose-file docker-compose.yaml up -d --build
