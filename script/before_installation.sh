1fb7a7c6f139f35534877080626ed37b3b31eacc
emmanuelagyapong <emmanuelagyapong070@gmail.com>
2020-06-09 22:10:31 +0000

    ansible

--- script/before_installation.sh
+++ script/before_installation.sh
@@ -7,6 +7,6 @@ mkdir -p ~/.local/bin
 echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
 source ~/.bashrc
 ## install ansible with pip
-pip3 install --user ansible
+pip3 install ansible
 # check that ansible has been installed
 ansible --version
