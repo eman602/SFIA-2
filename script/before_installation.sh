534c8b7e27cc2b8c52fea88a3dbba925fb8d5084
emmanuelagyapong <emmanuelagyapong070@gmail.com>
2020-06-10 08:59:34 +0000

    ansible

--- script/before_installation.sh
+++ script/before_installation.sh
@@ -4,8 +4,8 @@ sudo apt install python3 python3-venv python3-pip -y
 python3 -m venv venv
 source ~/bashrc
 mkdir -p ~/.local/bin
-echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
-source ~/.bashrc
+echo 'PATH=$PATH:~/.local/bin' >> ~/bashrc
+source ~/bashrc
 ## install ansible with pip
 pip install --user ansible
 # check that ansible has been installed
