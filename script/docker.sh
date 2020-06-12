e5650fecfba8f26b6db6d76a484a54eda0540966
emmanuelagyapong <emmanuelagyapong070@gmail.com>
2020-06-12 12:19:40 +0000

    jenkins

--- script/docker.sh
+++ script/docker.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 source ~/bashrc
 sudo chmod 666 /var/run/docker.sock
-
+docker build -t emmanuelagyapong/service_1:latest ./Service_1
+docker build -t emmanuelagyapong/service_2:latest ./Service_2
 docker build -t emmanuelagyapong/service_3:latest ./Service_3
+docker build -t emmanuelagyapong/service_3:latest ./Service_4
+
+
+docker push emmanuelagyapong/service_1
+docker push emmanuelagyapong/service_2
 docker push emmanuelagyapong/service_3
+docker push emmanuelagyapong/service_4
 
 docker stack deploy --compose-file docker-compose.yaml stackdemo
