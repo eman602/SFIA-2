d85ba1abb7ebcf89b393e0d0ee0ace314567b7b2
emmanuelagyapong <emmanuelagyapong070@gmail.com>
2020-06-12 12:20:32 +0000

    jenkins

--- script/docker.sh
+++ script/docker.sh
@@ -4,7 +4,7 @@ sudo chmod 666 /var/run/docker.sock
 docker build -t emmanuelagyapong/service_1:latest ./Service_1
 docker build -t emmanuelagyapong/service_2:latest ./Service_2
 docker build -t emmanuelagyapong/service_3:latest ./Service_3
-docker build -t emmanuelagyapong/service_3:latest ./Service_4
+docker build -t emmanuelagyapong/service_4:latest ./Service_4
 
 
 docker push emmanuelagyapong/service_1
