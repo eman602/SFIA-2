0c9692aca04356b001c2fdd0ebf775fb5f7825ef
emmanuelagyapong <emmanuelagyapong070@gmail.com>
2020-06-14 19:56:34 +0000

    reset

--- Service_3/application/routes.py
+++ Service_3/application/routes.py
@@ -4,10 +4,8 @@ import random
 
 @app.route('/buildingfullname2', methods=['GET'])
 def secondname():
-
     list2=['benson', 'hesketh', 'ahmed', 'jennifer', 'oskar']
-
     response2=list2[random.randint(0, len(list2)-1)]
 
-    return response2.upper()
+    return response2
     
